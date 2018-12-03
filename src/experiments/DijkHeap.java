a18a8f9b23ab4aa1409435667b684c22cc57f84b
Sebastian Donoso <sebastian.a.donoso.b@gmail.com>
2018-12-03 01:37:29 -0300

    first part dijkstra

--- src/experiments/DijkHeap.java
+++ src/experiments/DijkHeap.java
@@ -1,4 +1,44 @@
 package experiments;
 
+import javafx.util.Pair;
+import structs.Graph;
+import structs.Heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DijkHeap {
+    public void Dijsktra(double[][] graph, double origin) {
+        Heap Q = new Heap();
+        int rows = graph.length;
+        int cols = graph[0].length;
+        List<Pair<Integer, Integer>> dist, prev;
+        dist = new ArrayList<Pair<Integer, Integer>>();
+        prev = new ArrayList<Pair<Integer, Integer>>();
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; i < cols; j++) {
+                if (graph[i][j] == origin) {
+                    //dist[] =0;
+
+
+                } else {
+                    //dist[]=-1;//infinito
+                }
+                //prev[v] = null;
+                //Q.insert(v,dist[v]); //agrega a cola con distancia de origen como prioridad
+
+
+            }
+
+        }
+        while(!Q.isEmpty()){
+            //m = Q.extractMin();//sacar vertice con mayor prioridad
+            //cada vecino que sigue en Q
+            //necesito saber que nodos estan en el heap
+
+        }
+
+    }
 }
